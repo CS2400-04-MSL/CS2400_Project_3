202ace3d463595cc461adf2f80128501bce53516
Manji <Marco.w246@gmail.com>
2021-10-24 20:40:38 -0700

    Unit Test 1

---
+++ PostorderTraverseTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PostorderTraverseTest {
+
+	@Test
+	void test1() {
+		BinaryTree<String> aTree2 = new BinaryTree<>();
+		DriverBT.createTree2(aTree2);
+		
+	}
+
+}
---
+++ PostorderTraverse_callBinaryNodeMethodTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PostorderTraverse_callBinaryNodeMethodTest {
+
+	@Test
+	void test() {
+		BinaryTree<String> aTree2 = new BinaryTree<>();
+		DriverBT.createTree2(aTree2);
+		
+	}
+
+}
