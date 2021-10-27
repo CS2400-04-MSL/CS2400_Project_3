af9ab946914afb856148ea17367ec0a85e5ccce6
Manji <Marco.w246@gmail.com>
2021-10-26 18:54:40 -0700

    More JUnit tests

--- GetHeight_callBinaryNodeMethodTest.java
+++ GetHeight_callBinaryNodeMethodTest.java
@@ -2,13 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class PostorderTraverse_callBinaryNodeMethodTest {
+class GetHeight_callBinaryNodeMethodTest {
 
 	@Test
 	void test1() {
 		BinaryTree<String> aTree2 = new BinaryTree<>();
 		DriverBT.createTree2(aTree2);
-		
+		assertEquals(4, aTree2.getHeight_callBinaryNodeMethod());
 	}
 
 }
--- GetNumberOfNodesTest.java
+++ GetNumberOfNodesTest.java
@@ -2,13 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class PostorderTraverse_callBinaryNodeMethodTest {
+class GetNumberOfNodesTest {
 
 	@Test
 	void test1() {
 		BinaryTree<String> aTree2 = new BinaryTree<>();
 		DriverBT.createTree2(aTree2);
-		
+		assertEquals(8, aTree2.getNumberOfNodes());
 	}
 
 }
