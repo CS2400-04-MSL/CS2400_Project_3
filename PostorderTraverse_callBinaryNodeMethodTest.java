147707a2c2d36ac60ed0af31ccde72a9ba1159ae
Manji <Marco.w246@gmail.com>
2021-10-26 21:26:00 -0700

    Completed unit testing

--- PostorderTraverse_callBinaryNodeMethodTest.java
+++ PostorderTraverse_callBinaryNodeMethodTest.java
@@ -6,9 +6,14 @@ class PostorderTraverse_callBinaryNodeMethodTest {
 
 	@Test
 	void test1() {
-		BinaryTree<String> aTree2 = new BinaryTree<>();
-		DriverBT.createTree2(aTree2);
-		
+		BinaryTree<String> aTree1 = new BinaryTree<>();
+		DriverBT.createTree2(aTree1);
+		assertThrows(EmptyTreeException.class, () -> {aTree1.postorderTraverse_callBinaryNodeMethod(); });
 	}
-
+	
+	@Test
+	void test2() {
+		BinaryTree<String> aTree2 = new BinaryTree<>();
+		assertThrows(EmptyTreeException.class, () -> {aTree2.postorderTraverse_callBinaryNodeMethod(); });
+	}
 }
